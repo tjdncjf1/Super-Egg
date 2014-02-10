a425ba8d4d50ec09f3d02d00c9e10dfe802c4c95
Woochoel <suwoocher@naver.com>
2014-02-10 19:34:15 +0900

    .

--- Must/src/main/java/Hello.java
+++ Must/src/main/java/Hello.java
@@ -2,5 +2,10 @@
 public class Hello {
 	public static void main(String[] args) {
 	  System.out.println("hi");
+	  System.out.println("hi");
+	  System.out.println("hi");
+	  System.out.println("hi");
+	  System.out.println("hi");
+	  System.out.println("hi");
   }
 }
