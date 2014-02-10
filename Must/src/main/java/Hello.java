7c884005856138f67ea57ba81004fe60726ab44b
Woochoel <suwoocher@naver.com>
2014-02-10 21:08:17 +0900

    .

--- Must/src/main/java/Hello.java
+++ Must/src/main/java/Hello.java
@@ -26,6 +26,8 @@ public class Hello {
 	  System.out.println("hi3");
 	  System.out.println("hi3");
 	  System.out.println("hi4");
+	  System.out.println("hi5");
+	  System.out.println("hi5");
 	  
   }
 }
