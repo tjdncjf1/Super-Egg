038de798aabe05b26d7b3a7908a9c666421644f5
ahn sunhoo <dkswndtls@gmail.com>
2014-02-13 09:45:48 +0900

    .

--- Must/src/main/webapp/Chart.js
+++ Must/src/main/webapp/Chart.js
@@ -5,6 +5,7 @@
  * Copyright 2013 Nick Downie
  * Released under the MIT license
  * https://github.com/nnnick/Chart.js/blob/master/LICENSE.md
+ * 
  */
 
 //Define the global Chart Variable as a class.
