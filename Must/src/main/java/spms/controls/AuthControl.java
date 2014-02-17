2cff6c210bcb2a32535a97f25ae13f495eeafa0c
Woochoel <suwoocher@naver.com>
2014-02-17 17:08:17 +0900

    .

--- Must/src/main/java/spms/controls/AuthControl.java
+++ Must/src/main/java/spms/controls/AuthControl.java
@@ -2,63 +2,43 @@ package spms.controls;
 
 import java.util.HashMap;
 
-import javax.servlet.http.Cookie;
-import javax.servlet.http.HttpServletResponse;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.CookieValue;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.SessionAttributes;
 import org.springframework.web.bind.support.SessionStatus;
 
 import spms.dao.MemberDao;
-import spms.vo.Member;
+import spms.vo.JsonResult;
 
 @Controller
 @RequestMapping("/auth")
-@SessionAttributes("loginUser")
 public class AuthControl {
 
 	@Autowired(required=false)
 	MemberDao memberDao; 
 
-	@RequestMapping(value="/login", method=RequestMethod.GET)
-	public String form(@CookieValue(required=false) String email, Model model) {
-		if (email != null) {
-			model.addAttribute("email", email);
-			model.addAttribute("checkSaveEmail", "checked");
-		}
-		return "auth/login";
-	}
+	//	@RequestMapping(value="/login", method=RequestMethod.GET)
+	//	public String form(@CookieValue(required=false) String email, Model model) {
+	//		if (email != null) {
+	//			model.addAttribute("email", email);
+	//			model.addAttribute("checkSaveEmail", "checked");
+	//		}
+	//		return "auth/login";
+	//	}
 
-	@RequestMapping(value="/login", method=RequestMethod.POST)
-	public String login(String email, String password, String saveEmail,
-			HttpServletResponse response,
-			Model model) throws Exception {
+	@RequestMapping(value="/login", method=RequestMethod.POST,
+			produces="application/json")
+	public Object login(String email, String password) throws Exception {
 
-		Cookie cookie = null;
-		if (saveEmail != null) {
-			cookie = new Cookie("email", email);
-			cookie.setMaxAge(60 * 60 * 24 * 3);
-		} else {
-			cookie = new Cookie("email", null);
-			cookie.setMaxAge(0);
-		}
-		response.addCookie(cookie);
+		try {
+			HashMap<String, String> sqlparamMap = new HashMap<String,String>();
+			sqlparamMap.put("email", email);
+			sqlparamMap.put("password", password);
 
-		HashMap<String, String> sqlparamMap = new HashMap<String,String>();
-		sqlparamMap.put("email", email);
-		sqlparamMap.put("password", password);
-
-		Member member = memberDao.selectByEmailPassword(sqlparamMap);
-
-		if (member != null) {
-			model.addAttribute("loginUser", member);
-			return "redirect:../main.do";
-		} else {
+			memberDao.selectByEmailPassword(sqlparamMap);
+			return new JsonResult().setResultStatus(JsonResult.SUCCESS);
+		} catch (Throwable ex) {
 			return "auth/loginFail";
 		}
 	}
@@ -73,5 +53,5 @@ public class AuthControl {
 	public String loginUser() throws Exception {
 		return "auth/loginUser";
 	}
-	
+
 }
