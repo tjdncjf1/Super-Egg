e642de158664c9fb65d7684141ccffb0570185f4
Woochoel <suwoocher@naver.com>
2014-02-26 16:05:14 +0900

    ..

--- Must/src/main/webapp/js/accordion/list.js
+++ Must/src/main/webapp/js/accordion/list.js
@@ -1,11 +1,15 @@
 $(function() {
+	var list = '<div id="st-accordion" class="st-accordion">';
+	list += '<ul><li><a href="#">Photography<span class="st-arrow">Open or Close</span></a>';
+	list += '<div class="st-content">';
+	list += '<p>She packed her seven versalia, put her initial into the belt and made herself on the way.</p>';
+	list += '<img src="images/10.jpg" alt="image01"/></div></li></ul></div>';
+	
+	$('#list').append($(list));
+
 	$('#st-accordion').accordion({
 		oneOpenedItem : true
 	});
-	
-	
-	
-	
 	
 	
 });
