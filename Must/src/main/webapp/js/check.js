1bcd4a17ded1501fd62887b933310db3c102eccd
Woochoel <suwoocher@naver.com>
2014-04-01 18:38:14 +0900

    x

--- Must/src/main/webapp/js/check.js
+++ Must/src/main/webapp/js/check.js
@@ -34,32 +34,28 @@ $(function(){
 			},
 			password: {
 				required: true,
-				minlength: 8,
-				maxlength: 16,
+				minlength: 8
 			},
 			pwchk: {
 				required: true,
 				minlength: 8,
-				maxlength: 16,
 				equalTo: '#password'
 			}
 		},
 		messages: {
 			email: {
-				required: '이메일을 입력하세요',
-				email: '이메일을 바르게 입력하세요',
-				emailCheck: '중복된 이메일입니다.'
+				required: 'Enter Email!',
+				email: 'The email address you entered is not valid.',
+				emailCheck: 'Duplicated Email!'
 			},
 			password: {
-				required: '비밀번호를 입력하세요',
-				minlength: '8자 이상으로 입력하세요',
-				maxlength: '16자 이하로 입력하세요',
+				required: 'Enter Password!',
+				minlength: 'Choose a stronger password.'
 			},
 			pwchk: {
-				required: '비밀번호를 입력하세요',
-				minlength: '8자 이상으로 입력하세요',
-				maxlength: '16자 이하로 입력하세요',
-				equalTo: '비밀번호가 서로 일치하지 않습니다.'
+				required: 'Enter Password!',
+				minlength: 'Choose a stronger password.',
+				equalTo: 'Passwords do not match.'
 			}
 		},
 		submitHandler: function(){
